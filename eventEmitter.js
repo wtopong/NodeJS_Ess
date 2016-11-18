10b9a4e9b1f8a4d4cea239ed8f344478635f054f
wtopong <wtopong@hotmail.com>
2016-11-18 13:00:06 +1100

    Added new folder, lib and Person module : lesson 4.4

--- eventEmitter.js
+++ eventEmitter.js
@@ -9,20 +9,18 @@
 // emitter.emit('customerEvent', "Hello World", 200);
 
 // pull out EventEmitter constructor
-var EventEmitter = require('events').EventEmitter;
-var util = require('util');
-
-// create person prototype
-var Person = function(name) {
-	this.name = name;
-};
-
-util.inherits(Person, EventEmitter);
+var Person = require("./lib/Person");
 
 var ben = new Person("Ben Franklin");
+var george = new Person("George Washington");
 
 ben.on('speak', function(said) {
 	console.log(`${this.name}: ${said}`);
 });
 
+george.on('speak', function(said) {
+	console.log(`${this.name}: ${said}`);
+});
+
 ben.emit('speak', "You may delay, but time will not.");
+george.emit('speak', "King speach.");
