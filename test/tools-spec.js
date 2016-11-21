6022de5cbbee0a2d05fba40aab56e12cc30c32b7
wtopong <wtopong@hotmail.com>
2016-11-21 14:32:17 +1100

    Modified tools and tools-spec for asynchronous mocha testing

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,9 +1,25 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools");
 
-describe("printName()", function() {
-	it("should print the last name first", function() {
-		var results = tools.printName({first: "Alex", last: "Banks"});
-		expect(results).to.equal("Banks, Alex");
+describe("Tools", function() {
+
+	describe("printName()", function() {
+		it("should print the last name first", function() {
+			var results = tools.printName({first: "Alex", last: "Banks"});
+			expect(results).to.equal("Banks, Alex");
+		});
 	});
+
+	describe("loadWiki()", function() {
+		// by default is 2000
+		this.timeout(5000);
+		it("Load Abraham Lincoln's wikipedia page", function(done) {
+			tools.loadWiki({first: "Abraham", last: "Lincoln"}, function(html) {
+				expect(html).to.be.ok;
+				done();
+			});
+		});
+	});
+
 });
+
