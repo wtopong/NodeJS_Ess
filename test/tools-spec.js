b2a12478aaaeae18fa1ab3b79ffda3007ec517f5
wtopong <wtopong@hotmail.com>
2016-11-21 14:59:04 +1100

    Modified tools-spec for mockingaserver with nock lesson 10.3

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,5 +1,7 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools");
+// to create mockup wikipedia server 
+var nock = require("nock");
 
 describe("Tools", function() {
 
@@ -12,10 +14,18 @@ describe("Tools", function() {
 
 	describe("loadWiki()", function() {
 		// by default is 2000
-		this.timeout(5000);
+		// this.timeout(5000);
+
+		before(function() {
+			// create fake server so it replies with status 200
+			nock("https://en.wikipedia.org")
+				.get("/wiki/Abraham_Lincoln")
+				.reply(200, "Mock Abraham Linkcoln Page");
+		});
+
 		it("Load Abraham Lincoln's wikipedia page", function(done) {
 			tools.loadWiki({first: "Abraham", last: "Lincoln"}, function(html) {
-				expect(html).to.be.ok;
+				expect(html).to.equal("Mock Abraham Linkcoln Page");
 				done();
 			});
 		});
