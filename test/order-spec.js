666ed39c1307e01e6494b04c4ff0e302c54b9419
wtopong <wtopong@hotmail.com>
2016-11-21 22:29:06 +1100

    Add spy for warehouse to answer (mock warehouse) for lesson 10.6

--- test/order-spec.js
+++ test/order-spec.js
@@ -7,6 +7,7 @@ var order = rewire("../lib/order");
 var sinon = require("sinon");
 // use describe function for mocha module
 describe("Ordering Items", function() {
+	// create fake data for each hook up
 	beforeEach(function() {
 		this.testData  = [
 		// SKU Stock Keeping Unit
@@ -14,25 +15,49 @@ describe("Ordering Items", function() {
 			{sku: "BBB", qty: 0},
 			{sku: "CCC", qty: 3}
 		];
-
+		// mockup console
 		this.console = {
 			log: sinon.spy()
 		};
-
+		// mockup warehouse
+		this.warehouse = {
+			packageAndShip: sinon.stub().yields(10987654321)
+		};
 		order.__set__("inventoryData", this.testData);
 		// use rewire to inject the console
 		order.__set__("console", this.console);
+		// inject mock warehouse
+		order.__set__("warehouse", this.warehouse);
 	});
 
 	// it an asynchronous method
 	it("order an item when there are enough in stock", function(done) {
 		// to protect the scope of this
 		var _this = this;
-		// inject done function if it never to the line 33
-		// this test will fail
+		// inject done function if it never to the line 40 this test will fail
 		order.orderItem("CCC", 3, function() {
 			expect(_this.console.log.callCount).to.equal(2);
 			done();
 		});
 	});
+
+	describe("warehouse interaction", function() {
+
+		beforeEach(function() {
+			this.callback = sinon.spy();
+			// cannot wait the call-back function to be called so injecting with spy
+			order.orderItem("CCC", 2, this.callback);
+		});
+
+		it("receives a tracking number", function() {
+			expect(this.callback.calledWith(10987654321)).to.equal(true);
+		});
+
+		it("calls packageAndShip with the correct sku and quantity", function() {
+			expect(this.warehouse.packageAndShip.calledWith("CCC", 2)).to.equal(true);
+		});
+
+	});
+
 });
+
